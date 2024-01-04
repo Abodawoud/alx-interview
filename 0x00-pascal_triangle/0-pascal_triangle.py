1491f28babd74cea412b0531590836a58949fc3b
Abodawoud <yousefabodawoud@gmail.com>
2024-01-04 17:12:51 +0200

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,15 +14,13 @@ def pascal_triangle(n):
 
     triangle = []
     first_row = [1]
-    second_row = [1, 1]
 
     triangle.append(first_row)
-    triangle.append(second_row)
 
-    for i in range(1, n - 1):
+    for i in range(1, n):
         row = [1]
-        for j in range(len(triangle[i]) - 1):
-            row.append(triangle[i][j] + triangle[i][j + 1])
+        for j in range(len(triangle[i - 1]) - 1):
+            row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
         row.append(1)
         triangle.append(row)
 
