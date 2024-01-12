d5b99f62f621a1c576d635d7b979c77241629276
Abodawoud <yousefabodawoud@gmail.com>
2024-01-12 04:32:50 +0200

    enhance code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-""" Method that determines if all boxes can be opened """
+"""check if all boxes can be opened"""
 
 
 def canUnlockAll(boxes):
-    """ Method that determines if all boxes can be opened """
-
-    for k in range(1, len(boxes)):
-        flag = False
-        for box in range(len(boxes)):
-            if k in boxes[box] and box != k:
-                flag = True
-                break
-        if not flag:
-            return False
-
-    return True
+    """check if all boxes can be opened"""
+    keys = []
+    keys.append(0)
+    for box in range(len(boxes)):
+        for key in boxes[box]:
+            if key not in keys:
+                if key != box and key < len(boxes):
+                    keys.append(key)
+    if len(keys) == len(boxes):
+        return True
+    return False
