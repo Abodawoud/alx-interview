4605379299d3c9cd3e3895e60967d43cb32d9bf5
Abodawoud <yousefabodawoud@gmail.com>
2024-02-18 02:04:38 +0200

    starwars

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+request(
+  `https://swapi.dev/api/films/${movieId}/`,
+  function (error, response, body) {
+    if (error) {
+      console.error('Error fetching movie data:', error);
+      return;
+    }
+
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+
+    characters.reduce((prevPromise, characterUrl) => {
+      return prevPromise.then(() => {
+        return fetchAndPrintCharacterName(characterUrl);
+      });
+    }, Promise.resolve());
+  }
+);
+
+const fetchAndPrintCharacterName = (characterUrl) => {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, function (error, response, body) {
+      if (error) {
+        console.error('Error fetching character data:', error);
+        reject(error);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+      resolve();
+    });
+  });
+};
